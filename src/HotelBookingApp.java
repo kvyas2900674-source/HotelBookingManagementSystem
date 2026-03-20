5e8ec661f8146bb5bf80e42a16f56f6927027073
kvyas2900674-source <kvyas.2900674>
2026-03-20 09:04:32 +0530

    UC5: Implemented booking request queue using FIFO

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,50 +1,45 @@
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Hotel Booking Application
- * Use Case 4: Room Search & Availability Check
- * @version 4.0
+ * Use Case 5: Booking Request Queue (FIFO)
+ * @version 5.0
  */
+
+// Reservation class
+class Reservation {
+    String guestName;
+    String roomType;
+
+    Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+}
+
 public class HotelBookingApp {
 
     public static void main(String[] args) {
 
         System.out.println("===================================");
-        System.out.println(" Hotel Booking System v4.0 ");
+        System.out.println(" Hotel Booking System v5.0 ");
         System.out.println("===================================\n");
 
-        // Centralized Inventory (same as UC3)
-        HashMap<String, Integer> inventory = new HashMap<>();
-        inventory.put("Single", 5);
-        inventory.put("Double", 3);
-        inventory.put("Suite", 2);
+        // Queue for booking requests
+        Queue<Reservation> bookingQueue = new LinkedList<>();
 
-        // Room details (domain data)
-        HashMap<String, Integer> priceMap = new HashMap<>();
-        priceMap.put("Single", 2000);
-        priceMap.put("Double", 3500);
-        priceMap.put("Suite", 6000);
+        // Adding requests (FIFO order)
+        bookingQueue.add(new Reservation("Sanyam", "Single"));
+        bookingQueue.add(new Reservation("Rahul", "Double"));
+        bookingQueue.add(new Reservation("Priya", "Suite"));
 
-        HashMap<String, Integer> bedMap = new HashMap<>();
-        bedMap.put("Single", 1);
-        bedMap.put("Double", 2);
-        bedMap.put("Suite", 3);
+        System.out.println("Booking Requests in Queue:\n");
 
-        System.out.println("Available Rooms:\n");
-
-        // SEARCH LOGIC (READ ONLY)
-        for (String type : inventory.keySet()) {
-
-            int available = inventory.get(type);
-
-            // Show only available rooms
-            if (available > 0) {
-                System.out.println("Room Type: " + type);
-                System.out.println("Price: " + priceMap.get(type));
-                System.out.println("Beds: " + bedMap.get(type));
-                System.out.println("Available Rooms: " + available);
-                System.out.println();
-            }
+        // Display queue without removing
+        for (Reservation r : bookingQueue) {
+            System.out.println("Guest: " + r.guestName + " | Room Type: " + r.roomType);
         }
+
+        System.out.println("\nTotal Requests in Queue: " + bookingQueue.size());
     }
 }
